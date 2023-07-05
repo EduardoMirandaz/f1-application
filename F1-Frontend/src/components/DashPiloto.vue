c8e608a60a8a449a751d2c9439314dd843c12871
GustavoOM <gumartins2001@gmail.com>
2023-07-05 10:10:46 -0300

    Relatório 5 finalizado

--- F1-Frontend/src/components/DashPiloto.vue
+++ F1-Frontend/src/components/DashPiloto.vue
@@ -2,25 +2,51 @@
     <div class="dashboard">
         <h2 class="homeAdminGerenciarLabel ajusteMargin">Selecione o relatório</h2>
         <div class="gridHome">
-            <button class="btn btn-lg px-5 btn btn-success" @click="mudarParaORelatorio1()">Exibir resultados </button>
-            <button class="btn btn-lg px-5 btn btn-success" @click="mudarParaORelatorio2()">Aeroportos próximos no território brasileiro</button>
+            <button class="btn btn-lg px-5 btn btn-success" @click="mudarParaORelatorio1()">Exibir resultados das corridas </button>
+            <button class="btn btn-lg px-5 btn btn-success" @click="mudarParaORelatorio2()">to do</button>
         </div>
         <div class="relatorio" v-if="relatorio1">
-            <h3 class="relatorio1Desc">Esse relatório indica a quantidade de resultados por cada status, apresentando o
-nome do status e sua contagem.</h3>
+            <h3 class="relatorio1Desc">Esse relatório tem o objetivo de consultar a quantidade de vitórias obtidas, apresentando o ano e a corrida onde cada vitlória foi alcançada.</h3>
             <div class="ordenadores">
                 <h4>Ordenar por: </h4>
                 <div class="ordenador" @click="mudarOrdenador(0)" :class="{ 'selecionado': selecionado == 0}">
-                    <h4>Título do Status</h4>
+                    <h4>Visão Geral</h4>
                 </div>
                 <div class="ordenador" @click="mudarOrdenador(1)" :class="{ 'selecionado': selecionado == 1}">
-                    <h4>Quantidade</h4>
+                    <h4>Por Corrida</h4>
+                </div>
+                <div class="ordenador" @click="mudarOrdenador(2)" :class="{ 'selecionado': selecionado == 2}">
+                    <h4>Por Ano</h4>
                 </div>
             </div>
-            <div class="statusList">
-                <div v-for="status in statusList" :key="status.status" class="status">
-                    <h4 class="statusStatus">{{ status.status }}</h4>
-                    <h4 class="statusQuantity">{{ status.quantity }}</h4>
+            <div v-if="selecionado == 0" class="statusList">
+                <h3 class="subtitulo">Total de vitórias: <span class="label"> {{ resultadoPiloto.geral.length }}</span></h3>
+                <div v-for="resultado in resultadoPiloto.geral" :key="resultado.nomePremio" class="status">
+                    <h4 class="statusStatus"><span class="label">Ano</span></h4>
+                    <h4 class="statusQuantity">{{ resultado.ano }}</h4>
+                    <h4 class="statusStatus"><span class="label">Prêmio</span></h4>
+                    <h4 class="statusQuantity">{{ resultado.nomePremio }}</h4>
+                </div>
+            </div>
+            <div v-if="selecionado == 1" class="vitoriasPorCorrida">
+                <div v-for="resultado in resultadoPiloto.porCorrida" :key="resultado.nomeCorrida" class="statusPorCorrida">
+                    <h3 class="subtitulo"><span class="label"> {{ resultado.anos.length }}</span> vitória(s) na corrida <span class="label"> {{ resultado.nomeCorrida }}</span></h3> 
+                    <div class="anos">
+                        <h4 class="subtitulo">No(s) ano(s) de:</h4> 
+                        <div class="anos">
+                            <h4 v-for="ano in resultado.anos" :key="ano" class="statusQuantity"><span class="label"> {{ ano }}</span></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-if="selecionado == 2" class="vitoriasPorCorrida">
+                <div v-for="resultado in resultadoPiloto.porAno" :key="resultado.ano" class="statusPorCorrida">
+                    <h3 class="subtitulo"><span class="label"> {{ resultado.titulos.length }}</span> vitória(s) no ano de <span class="label"> {{ resultado.ano }}</span></h3> 
+                    <div class="anos">
+                        <div class="titulos">
+                            <h4 v-for="titulo in resultado.titulos" :key="titulo" class="statusQuantity"><span class="label"> {{ titulo }}</span></h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -76,14 +102,36 @@ nome do status e sua contagem.</h3>
             return {
                 relatorio1: false,
                 relatorio2: false,
-                selecionado: 1,
-                resultadoPiloto: {
-                    totalVitorias: 2,
+                selecionado: 0,
+                resultadoPiloto: {},
+            }
+        },
+        methods:{
+            mudarParaORelatorio1(){
+                this.relatorio1 = true
+                this.relatorio2 = false
+                this.selecionado = 0
+                this.resultadoPiloto = {
+                    geral:[
+                        {
+                            nomePremio: 'grampix',
+                            ano: 2022
+                        },
+                        {  
+                            nomePremio: 'grampix',
+                            ano: 2023
+                        },
+                        {
+                            nomePremio: 'interlagos',
+                            ano: 2022
+                        }
+                    ],
+                    
                     porAno:[
                         {
                             ano: 2022,
                             titulos: [
-                                'grampix'
+                                'grampix','interlagos'
                             ]
                         },
                         {
@@ -99,72 +147,15 @@ nome do status e sua contagem.</h3>
                             anos: [
                                 2022, 2023
                             ]
+                        },
+                        {
+                            nomeCorrida: 'interlagos',
+                            anos: [
+                                2022
+                            ]
                         }
                     ]
                 }
-            }
-        },
-        methods:{
-            mudarParaORelatorio1(){
-                this.relatorio1 = true
-                this.relatorio2 = false
-                this.selecionado = 1
-                this.cidades = []
-                this.statusList = [
-                    { "status": "Finished", "quantity": 7123 },
-                    { "status": "+1 Lap", "quantity": 3856 },
-                    { "status": "Engine", "quantity": 2014 },
-                    { "status": "+2 Laps", "quantity": 1594 },
-                    { "status": "Accident", "quantity": 1046 },
-                    { "status": "Did not qualify", "quantity": 1025 },
-                    { "status": "Collision", "quantity": 838 },
-                    { "status": "Gearbox", "quantity": 805 },
-                    { "status": "Spun off", "quantity": 792 },
-                    { "status": "+3 Laps", "quantity": 731 },
-                    { "status": "Suspension", "quantity": 431 },
-                    { "status": "+4 Laps", "quantity": 405 },
-                    { "status": "Did not prequalify", "quantity": 331 },
-                    { "status": "Transmission", "quantity": 321 },
-                    { "status": "Electrical", "quantity": 316 },
-                    { "status": "Brakes", "quantity": 251 },
-                    { "status": "Withdrew", "quantity": 244 },
-                    { "status": "+5 Laps", "quantity": 221 },
-                    { "status": "Clutch", "quantity": 214 },
-                    { "status": "Not classified", "quantity": 172 },
-                    { "status": "Fuel system", "quantity": 155 },
-                    { "status": "+6 Laps", "quantity": 153 },
-                    { "status": "Turbo", "quantity": 146 },
-                    { "status": "Disqualified", "quantity": 143 },
-                    { "status": "Hydraulics", "quantity": 138 },
-                    { "status": "Overheating", "quantity": 130 },
-                    { "status": "Ignition", "quantity": 128 },
-                    { "status": "Oil leak", "quantity": 123 },
-                    { "status": "Throttle", "quantity": 111 },
-                    { "status": "Out of fuel", "quantity": 100 },
-                    { "status": "+7 Laps", "quantity": 99 },
-                    { "status": "Halfshaft", "quantity": 99 },
-                    { "status": "Retired", "quantity": 95 },
-                    { "status": "Wheel", "quantity": 88 },
-                    { "status": "Oil pressure", "quantity": 87 },
-                    { "status": "Fuel pump", "quantity": 66 },
-                    { "status": "Differential", "quantity": 61 },
-                    { "status": "Tyre", "quantity": 55 },
-                    { "status": "Handling", "quantity": 54 },
-                    { "status": "+8 Laps", "quantity": 52 },
-                    { "status": "Fuel leak", "quantity": 50 },
-                    { "status": "Steering", "quantity": 47 },
-                    { "status": "Collision damage", "quantity": 45 },
-                    { "status": "Radiator", "quantity": 42 },
-                    { "status": "Puncture", "quantity": 41 },
-                    { "status": "Power Unit", "quantity": 41 },
-                    { "status": "+9 Laps", "quantity": 38 },
-                    { "status": "Wheel bearing", "quantity": 37 },
-                    { "status": "Injection", "quantity": 36 },
-                    { "status": "Fuel pressure", "quantity": 35 },
-                    { "status": "Water leak", "quantity": 32 },
-                    { "status": "+10 Laps", "quantity": 32 },
-                    { "status": "Alternator", "quantity": 31 }
-                ]
             },
             mudarParaORelatorio2(){
                 this.relatorio1 = false
@@ -175,21 +166,6 @@ nome do status e sua contagem.</h3>
             },
             mudarOrdenador(number){
                 this.selecionado = number
-                if(this.relatorio1){
-                    const keys = Object.keys(this.statusList[1]);
-                    const ordenador = keys[number]
-                    this.statusList = this.statusList.sort((a, b) => {
-                                                                const labelA = a[ordenador];
-                                                                const labelB = b[ordenador];
-                                                                if (labelA < labelB) {
-                                                                    return -1;
-                                                                }
-                                                                if (labelA > labelB) {
-                                                                    return 1;
-                                                                }
-                                                                return 0;}
-                                                            );
-                }
                 if(this.relatorio2){
                     const keys = Object.keys(this.cidades[1].aeroportosProximos[1]);
                     const ordenador = keys[number]
@@ -213,128 +189,6 @@ nome do status e sua contagem.</h3>
 
                 }
             },
-            buscar(){
-                this.cidades = 
-                [
-                   {
-                       "nome": "Palmas",
-                       "lat": -123122,
-                       "long": -312533,
-                       aeroportosProximos:[
-                           {
-                               "codigo_IATA": "MCZ",
-                               "aeroporto": "Zumbi dos Palmares Airport",
-                               "cidade_aeroporto": "Maceió",
-                               "distancia_em_km": 18.334337459440547,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "",
-                               "aeroporto": "Campo Délio Jardim de Mattos Airport",
-                               "cidade_aeroporto": "Rio de Janeiro",
-                               "distancia_em_km": 21.054782453529477,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "GRU",
-                               "aeroporto": "Guarulhos - Governador André Franco Montoro International Airport",
-                               "cidade_aeroporto": "São Paulo",
-                               "distancia_em_km": 21.46692425762662,
-                               "tipo_aeroporto": "large_airport"
-                           },
-                           {
-                               "codigo_IATA": "SSA",
-                               "aeroporto": "Deputado Luiz Eduardo Magalhães International Airport",
-                               "cidade_aeroporto": "Salvador",
-                               "distancia_em_km": 21.58471036151831,
-                               "tipo_aeroporto": "large_airport"
-                           },
-                           {
-                               "codigo_IATA": "CNF",
-                               "aeroporto": "Tancredo Neves International Airport",
-                               "cidade_aeroporto": "Belo Horizonte",
-                               "distancia_em_km": 31.88609470224482,
-                               "tipo_aeroporto": "large_airport"
-                           },
-                           {
-                               "codigo_IATA": "IZA",
-                               "aeroporto": "Presidente Itamar Franco Airport",
-                               "cidade_aeroporto": "Juiz de Fora",
-                               "distancia_em_km": 33.42876683533632,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "SNZ",
-                               "aeroporto": "Santa Cruz Air Force Base",
-                               "cidade_aeroporto": "Rio de Janeiro",
-                               "distancia_em_km": 55.122191487156186,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "TMT",
-                               "aeroporto": "Trombetas Airport",
-                               "cidade_aeroporto": "Oriximiná",
-                               "distancia_em_km": 66.56520341281421,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "MEU",
-                               "aeroporto": "Monte Dourado - Serra do Areão Airport",
-                               "cidade_aeroporto": "Almeirim",
-                               "distancia_em_km": 70.5572440938035,
-                               "tipo_aeroporto": "medium_airport"
-                           }
-                        ]
-                    }, 
-                    {
-                       "nome": "Palmas",
-                       "lat": -123122,
-                       "long": -312533,
-                       aeroportosProximos:[
-                           {
-                               "codigo_IATA": "CWB",
-                               "aeroporto": "Afonso Pena Airport",
-                               "cidade_aeroporto": "Curitiba",
-                               "distancia_em_km": 14.874290457270122,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "RBR",
-                               "aeroporto": "Rio Branco-Plácido de Castro International Airport",
-                               "cidade_aeroporto": "Rio Branco",
-                               "distancia_em_km": 14.94098487967697,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "ROO",
-                               "aeroporto": "Maestro Marinho Franco Airport",
-                               "cidade_aeroporto": "Rondonópolis",
-                               "distancia_em_km": 15.970973936232529,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "JTC",
-                               "aeroporto": "Bauru/Arealva–Moussa Nakhal Tobias State Airport",
-                               "cidade_aeroporto": "Bauru",
-                               "distancia_em_km": 17.168897982419114,
-                               "tipo_aeroporto": "medium_airport"
-                           },
-                           {
-                               "codigo_IATA": "NAT",
-                               "aeroporto": "São Gonçalo do Amarante - Governador Aluízio Alves International Airport",
-                               "cidade_aeroporto": "Natal",
-                               "distancia_em_km": 17.62816730753729,
-                               "tipo_aeroporto": "medium_airport"
-                           }
-                        ]
-                    }, 
-                ]
-            },
-            handleKeyPress(event) {
-                if (event.key === 'Enter') {
-                    this.buscar();
-                }
-            }
         }
     }
 </script>
@@ -422,14 +276,25 @@ nome do status e sua contagem.</h3>
         width: auto;
         border: 3px solid #888888;
         padding: 10px;
-        margin-bottom: 20px;
+        margin-bottom: 40px;
+        justify-content: center;
+        align-items: center;
     }
 
     /* Estilos para cada item de status */
     .status {
         display: flex;
         justify-content: space-between;
-        align-items: center;
+        align-items: start;
+        margin-bottom: 10px;
+        padding: 5px;
+    }
+
+    .statusPorCorrida {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: start;
         margin-bottom: 10px;
         padding: 5px;
     }
@@ -443,7 +308,8 @@ nome do status e sua contagem.</h3>
     /* Estilos para a quantidade do status */
     .statusQuantity {
     font-size: 24px;
-    color: #888888;
+    color: #222;
+    margin: 0 10px;
     }
 
     .cidades{
@@ -463,5 +329,26 @@ nome do status e sua contagem.</h3>
 
     .label{
         font-weight: bold;
+    }
+
+    .subtitulo{
+        margin-bottom: 20px;
+    }
+
+    .anos{
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 20px;
+    }
+
+    .vitoriasPorCorrida{
+        display: flex;
+        flex-direction: column;
+        width: auto;
+        border: 3px solid #888888;
+        padding: 10px;
+        margin-bottom: 40px;
+        justify-content: initial;
+        align-items: initial;
     }
 </style>
